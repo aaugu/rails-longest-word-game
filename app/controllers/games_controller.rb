ca670c880ce95ebbeaa4aa65f7a28ab9a792e469
Anais Augu <augu.anais@gmail.com>
2022-02-03 21:44:06 +0100

    score in progress

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,37 @@
-class GamesController < ApplicationController
+require 'open-uri'
+require 'json'
 
+class GamesController < ApplicationController
   def new
-    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+    @grid = Array.new(10) { ('A'..'Z').to_a.sample }
   end
 
   def score
     @word = params[:word]
+
+    if included?(@word, @grid) == false
+      @start = "Sorry but "
+      @end = " can't be build out of "
+    elsif english_word?(@word) == false
+      @start = "Sorry but "
+      @end = " doesn't seem to be a valid English word..."
+    elsif english_word?(@word) && included?(@word, @grid)
+      @start = "Congratulations! "
+      @end = " is a valid English word!"
+    end
   end
 
+  def english_word?(word)
+    url = "https://wagon-dictionary.herokuapp.com/#{word}"
+    data = JSON.parse(URI.open(url).read)
+    data["found"]
+  end
+
+def included?(word, grid)
+  word_test = word.chars
+
+  word_test.all? do |letter|
+    word_test.count(letter) <= grid.count(letter)
+  end
+end
 end
